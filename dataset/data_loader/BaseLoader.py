785a9499af788300af3be0cbfbadaa66d9bd4cb0
Javad <54026703+JVD9kh96@users.noreply.github.com>
2023-07-22 16:31:26 +0330

    Update BaseLoader.py

--- dataset/data_loader/BaseLoader.py
+++ dataset/data_loader/BaseLoader.py
@@ -416,7 +416,7 @@ class BaseLoader(Dataset):
             count += 1
         return input_path_name_list, label_path_name_list
 
-    def multi_process_manager(self, data_dirs, config_preprocess, multi_process_quota=2):
+    def multi_process_manager(self, data_dirs, config_preprocess, multi_process_quota=8):
         """Allocate dataset preprocessing across multiple processes.
 
         Args:
