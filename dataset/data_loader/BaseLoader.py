8ed747dcd136ab14a708fcf7e97f1b4e69ad2b98
Javad <54026703+JVD9kh96@users.noreply.github.com>
2023-08-29 21:29:17 +0330

    Update BaseLoader.py

--- dataset/data_loader/BaseLoader.py
+++ dataset/data_loader/BaseLoader.py
@@ -394,7 +394,7 @@ class BaseLoader(Dataset):
                     face_region, frame, prev_kps = BaseLoader.retina_detect_align(frame,
                                                         kps_all[0], prev_kps)
                     # print(face_region)
-                    face_region = face_region[0][0] if len(face_region)>1 else face_region[0]
+                    face_region = face_region[0] if len(face_region)>1 else face_region[0]
                         # print(face_region)
                     frames[i] = frame
                     face_region_all.append(face_region)
